c147b395a81e6a9a3abd38873e406e1b35c24349
unknown <yasmin.xworkz@gmail.com>
2023-01-10 09:11:27 +0530

    @component

--- mavenn/src/com/xworkz/mavenn/boot/ChocolateRunner.java
+++ mavenn/src/com/xworkz/mavenn/boot/ChocolateRunner.java
@@ -21,35 +21,36 @@ public class ChocolateRunner {
 	
 		ApplicationContext app=new AnnotationConfigApplicationContext(SpringConfiguration.class);
 		Chocolates sweet=app.getBean(Chocolates.class);
-		System.out.println(sweet);
-		
-		Bag b=app.getBean(Bag.class);
-		System.out.println(b);
-		
-		Bottle drink=app.getBean(Bottle.class);
-		System.out.println(drink);
-		
-		Bus travel=app.getBean(Bus.class);
-		System.out.println(travel);
-		
-		Cake bake=app.getBean(Cake.class);
-		System.out.println(bake);
-		
-		Chair sit=app.getBean(Chair.class);
-		System.out.println(sit);
-		
-		Hotel food=app.getBean(Hotel.class);
-		System.out.println(food);
-
-		Mask mask=app.getBean(Mask.class);
-		System.out.println(mask);
-		
-		Traffic safety=app.getBean(Traffic.class);
-		System.out.println(safety);
-		
-		Xworkz learn=app.getBean(Xworkz.class);
-		System.out.println(learn);
-		
+		String color=sweet.flavour("blue", 20);
+		System.out.println(color);
+//		
+//		Bag b=app.getBean(Bag.class);
+//		System.out.println(b);
+//		
+//		Bottle drink=app.getBean(Bottle.class);
+//		System.out.println(drink);
+//		
+//		Bus travel=app.getBean(Bus.class);
+//		System.out.println(travel);
+//		
+//		Cake bake=app.getBean(Cake.class);
+//		System.out.println(bake);
+//		
+//		Chair sit=app.getBean(Chair.class);
+//		System.out.println(sit);
+//		
+//		Hotel food=app.getBean(Hotel.class);
+//		System.out.println(food);
+//
+//		Mask mask=app.getBean(Mask.class);
+//		System.out.println(mask);
+//		
+//		Traffic safety=app.getBean(Traffic.class);
+//		System.out.println(safety);
+//		
+//		Xworkz learn=app.getBean(Xworkz.class);
+//		System.out.println(learn);
+//		
 		
 
 	}
--- mavenn/src/com/xworkz/mavenn/type/Chocolates.java
+++ mavenn/src/com/xworkz/mavenn/type/Chocolates.java
@@ -7,8 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Chocolates {
 	
+	public String color;
+	public int price;
+	
 	public Chocolates() {
 	           System.out.println("kitkat is more tastier than other chocolates");
+	}
+	
+	public String flavour(String color,int price) {
+		this.color=color;
+		this.price=price;
+		return color;
 	}
 
 }
